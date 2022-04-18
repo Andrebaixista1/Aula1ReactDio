865d0c73faedfa23519d69ed877a8e8db4ddd542
unknown <andrbter@gmail.com>
2022-04-18 16:43:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Item from "./components/Item";
+
+const App = () => {
+  return (
+    <>
+      <h1>Minha primeira aplicação em React</h1>
+      <ul>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3
+        </Item>
+        
+      </ul>
+    </>
+  );
+};
+
+export default App;
